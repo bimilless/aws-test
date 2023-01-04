8fd4989d47b6932240980d4f2afaf39ca0e60e17
Seongmin <109591744+bimilless@users.noreply.github.com>
2023-01-04 17:33:58 +0900

    Add files via upload

---
+++ src/main/java/main/helloworld/Controller.java
@@ -0,0 +1,13 @@
+package main.helloworld;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+
+    @GetMapping("/")
+    public String hello() {
+        return "hello";
+    }
+}
---
+++ src/test/java/main/helloworld/TestApplicationTests.java
@@ -0,0 +1,13 @@
+package main.helloworld;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
